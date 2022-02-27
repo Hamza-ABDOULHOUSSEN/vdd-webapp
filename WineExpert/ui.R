2dd2e509849a6f25ecf69f29190fb06cb3be1515
ABDOULHOUSSEN Hamza <hamza.abdoulhoussen@telecomnancy.eu>
2022-02-27 11:39:34 +0100

    Ajout de box dans les pages

--- WineExpert/ui.R
+++ WineExpert/ui.R
@@ -26,35 +26,48 @@ dashboardPage(
       
       tabItem(tabName = "TD",
               h2("TD"),
-              plotOutput("gaussienne"),
-              plotOutput("sinus"),
-              numericInput(
-                "n",
-                "n",
-                value=25,
-                min = 0,
-                max = NA,
-                step = 1,
-                width = NULL
-              ),
-              checkboxInput("check", "relier les points", value = FALSE, width = NULL),
-              radioButtons(
-                "button",
-                "couleurs",
-                choices =  c("rouge" = "red",
-                             "vert" = "green",
-                             "bleu" = "blue"),
-                selected = NULL,
-                inline = FALSE,
-                width = NULL
-              ),
-              sliderInput("epaisseur", "Decimal:",
-                          min = 1, max = 5,
-                          value = 0.5, step = 0.1
-              ),
               
-              # Button
-              downloadButton("downloadImage", "Download")
+              box(
+                title = "Gauss", status = "primary", solidHeader = TRUE,
+                collapsible = TRUE,
+                plotOutput("gaussienne")
+              ),
+              box(
+                title = "Sinus", status = "primary", solidHeader = TRUE,
+                collapsible = TRUE,
+                plotOutput("sinus")
+              ),
+              box(
+                title = "Inputs", background = "black",
+                numericInput(
+                  "n",
+                  "n",
+                  value=25,
+                  min = 0,
+                  max = NA,
+                  step = 1,
+                  width = NULL
+                ),
+                checkboxInput("check", "relier les points", value = FALSE, width = NULL),
+                radioButtons(
+                  "button",
+                  "couleurs",
+                  choices =  c("rouge" = "red",
+                               "vert" = "green",
+                               "bleu" = "blue"),
+                  selected = NULL,
+                  inline = FALSE,
+                  width = NULL
+                ),
+                sliderInput("epaisseur", "Decimal:",
+                            min = 1, max = 5,
+                            value = 0.5, step = 0.1
+                ),
+                
+                # Button
+                downloadButton("downloadImage", "Download")
+              )
+              
       )
     )
   )
