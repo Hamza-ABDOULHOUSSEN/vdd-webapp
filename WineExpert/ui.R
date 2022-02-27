051e3ed78fdbedb72dc3a7f5c4a6387082f54534
ABDOULHOUSSEN Hamza <hamza.abdoulhoussen@telecomnancy.eu>
2022-02-27 11:30:37 +0100

    Ajout de pages

--- WineExpert/ui.R
+++ WineExpert/ui.R
@@ -8,40 +8,56 @@ dashboardPage(
   
   ## SIDEBAR
   dashboardSidebar(
-      numericInput(
-        "n",
-        "n",
-        value=25,
-        min = 0,
-        max = NA,
-        step = 1,
-        width = NULL
-      ),
-      checkboxInput("check", "relier les points", value = FALSE, width = NULL),
-      radioButtons(
-        "button",
-        "couleurs",
-        choices =  c("rouge" = "red",
-                     "vert" = "green",
-                     "bleu" = "blue"),
-        selected = NULL,
-        inline = FALSE,
-        width = NULL
-      ),
-      sliderInput("epaisseur", "Decimal:",
-                  min = 1, max = 5,
-                  value = 0.5, step = 0.1
-      ),
-      
-      # Button
-      downloadButton("downloadImage", "Download")
+    sidebarMenu(
+      menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+      menuItem("TD", icon = icon("th"), tabName = "TD",
+               badgeLabel = "new", badgeColor = "green")
+    )
   ),
   
   
   ## BODY
   dashboardBody(
-    plotOutput("histo"),
-    plotOutput("gaussienne"),
-    plotOutput("sinus")
+    tabItems(
+      tabItem(tabName = "dashboard",
+              h2("Dashboard tab content"),
+              plotOutput("histo")
+      ),
+      
+      tabItem(tabName = "TD",
+              h2("TD"),
+              plotOutput("gaussienne"),
+              plotOutput("sinus"),
+              numericInput(
+                "n",
+                "n",
+                value=25,
+                min = 0,
+                max = NA,
+                step = 1,
+                width = NULL
+              ),
+              checkboxInput("check", "relier les points", value = FALSE, width = NULL),
+              radioButtons(
+                "button",
+                "couleurs",
+                choices =  c("rouge" = "red",
+                             "vert" = "green",
+                             "bleu" = "blue"),
+                selected = NULL,
+                inline = FALSE,
+                width = NULL
+              ),
+              sliderInput("epaisseur", "Decimal:",
+                          min = 1, max = 5,
+                          value = 0.5, step = 0.1
+              ),
+              
+              # Button
+              downloadButton("downloadImage", "Download")
+      )
+    )
   )
+  
+  
 )
