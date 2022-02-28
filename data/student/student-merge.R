d62da5b3729ed9765b4259127f72ef84b6d9227c
ABDOULHOUSSEN Hamza <hamza.abdoulhoussen@telecomnancy.eu>
2022-02-28 21:47:54 +0100

    Open data from main directory

--- data/student/student-merge.R
+++ data/student/student-merge.R
@@ -1,5 +1,9 @@
-d1=read.table("data/student/student-mat.csv",sep=";",header=TRUE)
-d2=read.table("data/student/student-por.csv",sep=";",header=TRUE)
+mat=read.table("data/student/student-mat.csv",sep=";",header=TRUE)
+por=read.table("data/student/student-por.csv",sep=";",header=TRUE)
+student=merge(mat,por,by=c("school","sex","age","address","famsize","Pstatus","Medu","Fedu","Mjob","Fjob","reason","nursery","internet"))
 
-d3=merge(d1,d2,by=c("school","sex","age","address","famsize","Pstatus","Medu","Fedu","Mjob","Fjob","reason","nursery","internet"))
-print(nrow(d3)) # 382 students
+print(nrow(student)) # 382 students
+
+
+
+
